69a1d17617e07b6e5475288c6860d25350162109
vanessa_scappaticci@hotmail.co.uk <vanessa_scappaticci@hotmail.co.uk>
2018-07-30 17:28:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
   // Write your JavaScript code inside the init() function
-  
+function init() {
+ 
+}
+
 // Key codes for A, B, and C keys.
 const alphabet = [65, 66, 67];
  
