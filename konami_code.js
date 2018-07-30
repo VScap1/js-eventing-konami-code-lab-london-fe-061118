68f3689baf26db21523680e52a1330379dd45593
vanessa_scappaticci@hotmail.co.uk <vanessa_scappaticci@hotmail.co.uk>
2018-07-30 17:42:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,30 +17,10 @@ function init() {
   } else {
     index = 0;
   }
-}
  });
  }
-
-// Key codes for A, B, and C keys.
  
 // Keep track of index outside of the event handler.
-let index = 0;
  
 // This is the function that would be invoked by the event listener.
 
-const input = document.body;
-input.addEventListener (function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
-  if (key === alphabet[index]) {
-    index++;
-  if (index === alphabet.length) {
-      alert("Hurray!");
-      index = 0;
-    }
-  } else {
-    index = 0;
-
-  }
-}
-
-)
